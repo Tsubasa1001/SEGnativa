a80213fb8d63de7e747b97885076affca66a2223
cinisarboris <eyver.evm@gmail.com>
2021-03-20 21:34:05 -0400

    udpate : Inserción de datos [BD]

--- database/seeders/PacienteSeeder.php
+++ database/seeders/PacienteSeeder.php
@@ -2,17 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\Paciente;
 use Illuminate\Database\Seeder;
 
-class PacienteSeeder extends Seeder
-{
+class PacienteSeeder extends Seeder{
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(){
+        for ($pk=1; $pk <= 50; $pk++) {
+            Paciente::factory()->create([
+                'id' => $pk,
+                'codigo' => 'NT'.$pk,
+                'especialidad' => 0,
+                ]);
+        }
     }
 }
